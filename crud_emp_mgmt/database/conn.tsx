5ab58ffb46ab9acea83c4094cbc7b8f42d3f2af8
manas5996 <manas5996@gmail.com>
2023-02-26 21:01:16 +0530

    code commit!

--- crud_emp_mgmt/database/conn.tsx
+++ crud_emp_mgmt/database/conn.tsx
@@ -1,10 +1,11 @@
-const MONGO_URI =
-  "mongodb+srv://manas5996:Mkj5996@eplantdb.25bbqlq.mongodb.net/emp-mgmt-platform?retryWrites=true&w=majority";
+const URI = process.env.MONGO_URI;
+
 import mongoose from "mongoose";
 
 const connectMongo = async () => {
   try {
-    const { connection } = await mongoose.connect(MONGO_URI);
+    // console.log(URI);
+    const { connection } = await mongoose.connect(URI);
 
     if (connection.readyState == 1) {
       console.log("Database Connected");
