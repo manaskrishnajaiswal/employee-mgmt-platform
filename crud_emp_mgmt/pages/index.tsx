24f418adcb0f112c995a45188fb72974e4471b6f
manas5996 <manas5996@gmail.com>
2023-02-26 18:01:56 +0530

    code commit!

--- crud_emp_mgmt/pages/index.tsx
+++ crud_emp_mgmt/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Table from "@/components/table";
+import { BiUserPlus } from "react-icons/bi";
 
 export default function Home() {
   return (
@@ -13,6 +15,19 @@ export default function Home() {
         <h1 className="text-xl md:text-5xl text-center font-bold py-10">
           Employee Management
         </h1>
+        <div className="container mx-auto flex justify-between py-5 border-b">
+          <div className="left flex gap-3">
+            <button className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-gray-800">
+              Add Employee{" "}
+              <span className="px-1">
+                <BiUserPlus size={23}></BiUserPlus>
+              </span>
+            </button>
+          </div>
+          {/* collapseable form */}
+        </div>
+        {/* table */}
+        <Table></Table>
       </main>
     </>
   );
