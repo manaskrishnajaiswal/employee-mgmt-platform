9ec020c105982b8588ea44dc1b1120c780d99dde
manas5996 <manas5996@gmail.com>
2023-02-27 10:58:29 +0530

    code commit!

--- crud_emp_mgmt/lib/helper.tsx
+++ crud_emp_mgmt/lib/helper.tsx
@@ -1,4 +1,5 @@
 const BASE_URL = "http://localhost:3000";
+
 export const getUsersFromDB = async () => {
   const response = await fetch(`${BASE_URL}/api/users`);
   const json = await response.json();
