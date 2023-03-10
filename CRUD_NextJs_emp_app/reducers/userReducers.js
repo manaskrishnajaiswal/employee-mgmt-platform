378943dbe75c2ae406a30035a2de08e27079ed03
manas5996 <manas5996@gmail.com>
2023-03-10 15:33:04 +0530

    code commit!

--- CRUD_NextJs_emp_app/reducers/userReducers.js
+++ CRUD_NextJs_emp_app/reducers/userReducers.js
@@ -1,3 +1,26 @@
+import {
+  SINGLE_USER_GET_FAIL,
+  SINGLE_USER_GET_REQUEST,
+  SINGLE_USER_GET_RESET,
+  SINGLE_USER_GET_SUCCESS,
+  USER_CREATE_FAIL,
+  USER_CREATE_REQUEST,
+  USER_CREATE_RESET,
+  USER_CREATE_SUCCESS,
+  USER_DELETE_FAIL,
+  USER_DELETE_REQUEST,
+  USER_DELETE_RESET,
+  USER_DELETE_SUCCESS,
+  USER_GET_FAIL,
+  USER_GET_REQUEST,
+  USER_GET_RESET,
+  USER_GET_SUCCESS,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_RESET,
+  USER_UPDATE_SUCCESS,
+} from "../constants/userConstants";
+
 export const toggleChangeReducer = (state, action) => {
   state.client.toggleForm = !state.client.toggleForm;
 };
@@ -12,4 +35,83 @@ export const deleteUserReducer = (state, action) => {
 
 export const columnTypeReducer = (state, action) => {
   state.client.columnType = action.payload;
+};
+
+export const userDataCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_CREATE_REQUEST:
+      return { loading: true };
+    case USER_CREATE_SUCCESS:
+      return { loading: false, userdatacreate: action.payload };
+    case USER_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_CREATE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userDataGetReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_GET_REQUEST:
+      return { loading: true };
+    case USER_GET_SUCCESS:
+      return { loading: false, userdataget: action.payload };
+    case USER_GET_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_GET_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userDataDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true };
+    case USER_DELETE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        userdatadelete: action.payload,
+      };
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_DELETE_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userSingleDataGetReducer = (state = {}, action) => {
+  switch (action.type) {
+    case SINGLE_USER_GET_REQUEST:
+      return { loading: true };
+    case SINGLE_USER_GET_SUCCESS:
+      return { loading: false, usersingledataget: action.payload };
+    case SINGLE_USER_GET_FAIL:
+      return { loading: false, error: action.payload };
+    case SINGLE_USER_GET_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userSingleDataUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_UPDATE_REQUEST:
+      return { loading: true };
+    case USER_UPDATE_SUCCESS:
+      return { loading: false, usersingledataupdate: action.payload };
+    case USER_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_UPDATE_RESET:
+      return {};
+    default:
+      return state;
+  }
 };
