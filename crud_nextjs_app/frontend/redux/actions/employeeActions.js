c8bf4d85cf0e8869961bcf65ccf967ff7bc8f298
manas5996 <manas5996@gmail.com>
2023-03-18 21:00:21 +0530

    Added employee creation action and tested also

--- crud_nextjs_app/frontend/redux/actions/employeeActions.js
+++ crud_nextjs_app/frontend/redux/actions/employeeActions.js
@@ -3,6 +3,9 @@ import {
   EMPLOYEES_GET_FAIL,
   EMPLOYEES_GET_REQUEST,
   EMPLOYEES_GET_SUCCESS,
+  EMPLOYEE_CREATE_FAIL,
+  EMPLOYEE_CREATE_REQUEST,
+  EMPLOYEE_CREATE_SUCCESS,
 } from "../constants/employeeConstants";
 
 // GET /api/employee -> Get all employee in the company
@@ -25,6 +28,34 @@ export const employeesGetAction = () => async (dispatch) => {
     // console.log(error);
     dispatch({
       type: EMPLOYEES_GET_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// POST /api/employee -> Post/add employee in the company
+export const employeeCreateAction = (outputFormData) => async (dispatch) => {
+  try {
+    dispatch({
+      type: EMPLOYEE_CREATE_REQUEST,
+    });
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const { data } = await axios.post("/api/employee", outputFormData, config);
+    dispatch({
+      type: EMPLOYEE_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    // console.log(error);
+    dispatch({
+      type: EMPLOYEE_CREATE_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- crud_nextjs_app/pages/index.js
+++ crud_nextjs_app/pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BiUserPlus, BiX, BiCheck } from "react-icons/bi";
+import AddUserForm from "@/frontend/components/AddUserForm";
 
 export default function Home() {
   const [deleteId, setDeleteId] = useState("");
@@ -43,21 +44,33 @@ export default function Home() {
           <h1 className="text-xl md:text-5xl text-center font-bold py-10">
             Employee Management
           </h1>
-        </main>
-        <div className="container mx-auto flex justify-between py-5 border-b">
-          <div className="left flex gap-3">
-            <button
-              onClick={addEmployeehandler}
-              className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
-            >
-              Add Employee{" "}
-              <span className="px-1">
-                <BiUserPlus size={23}></BiUserPlus>
-              </span>
-            </button>
+          <div className="container mx-auto flex justify-between py-5 border-b">
+            <div className="left flex gap-3">
+              <button
+                onClick={addEmployeehandler}
+                className="flex bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800"
+              >
+                Add Employee{" "}
+                <span className="px-1">
+                  <BiUserPlus size={23}></BiUserPlus>
+                </span>
+              </button>
+            </div>
+            {deleteId ? (
+              DeleteComponent({ deletehandler, cancelhandler })
+            ) : (
+              <></>
+            )}
           </div>
-          {deleteId ? DeleteComponent({ deletehandler, cancelhandler }) : <></>}
-        </div>
+          {/* collapsable form */}
+          <div className="container mx-auto py-5">
+            {visible ? (
+              <AddUserForm visible={visible} setVisiblehandler={setVisible} />
+            ) : (
+              <></>
+            )}
+          </div>
+        </main>
       </section>
     </>
   );
